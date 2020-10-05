95f490760e45d11946180f8ca84c5a2b1179e822
wataru <dossjakapoko@gmail.com>
2020-10-05 14:43:07 +0900

    delete ContactComponents

--- src/components/organisms/index.jsx
+++ src/components/organisms/index.jsx
@@ -1,5 +1,4 @@
 export { default as About } from './About';
-export { default as Contact } from './Contact';
 export { default as Heading } from './Heading';
 export { default as Profile } from './Profile';
 export { default as SkillSet } from './SkillSet';
